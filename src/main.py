a01a179e4303588f8f9aa75673ead2b1838c9a23
AhmedLabib98 <Ahmed.labib.de@gmail.com>
2024-10-04 17:53:43 +0200

    Submission

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 # Import packages
-from data_loading import data_loading
-from concate import concatenate
-from encoding import basic_encoding
-from train import train_model
-from selection import select_features
-from predict import predict
-from f1_score import f1
+from src.data_loading import data_loading
+from src.concate import concatenate
+from src.encoding import basic_encoding
+from src.train import train_model
+from src.selection import select_features
+from src.predict import predict
+from src.f1_score import f1
 from sklearn.ensemble import RandomForestClassifier
+import pandas as pd
 
 # Load the data
 train_values, train_labels, test_values = data_loading()
@@ -40,5 +41,27 @@ y_pred_train, y_pred_test = predict(my_model, selected_features_df)
 # Evaluate the model
 f1(train_labels=train_labels, predictions=y_pred_train)
 
+# Converting y_pred_test to int 
+y_pred_test = y_pred_test.astype(int)
+
+# Converting the predictions to a pandas DataFrame
+y_pred_test_df = pd.DataFrame(y_pred_test)
+"""
+#y_pred_test_with_id = pd.concat([selected_features_df['building_id'].reset_index(drop = True)])
+Building_id = encod_features.loc[encod_features.type==0, "building_id"]
+
+y_pred_test.concat(Building_id)
+
+#y_pred_test.concat('building_id')
+
 # Create submission
-# TODO: Create submission
+frames = [Building_id, y_pred_test_df]
+result = pd.concat(frames)
+print(result)
+"""
+
+y_pred_test_df_flat = y_pred_test_df.values.ravel()
+
+submission = pd.DataFrame({'building_id': test_values['building_id'], 'damage_grade': y_pred_test_df_flat})
+
+pd.DataFrame.to_csv(submission, '1stSub.csv', index = False)
---
+++ src/submission.py
@@ -0,0 +1,4 @@
+submis_format = pd.read_csv(DATA_DIR / 'submission_format.csv', index_col = 'building_id')
+
+my_submission = pd.DataFrame(data = 
+                             )
